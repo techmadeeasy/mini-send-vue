9ffac5e581cb28f64370283c543ca39ef468fc86
jerryboy <djerryboy2@gmail.com>
2021-02-16 08:30:50 +0200

    a nice message on the home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,20 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      <b-jumbotron bg-variant="info" text-variant="white" border-variant="dark">
+        <template #header>Mini Send</template>
+
+        <template #lead>
+          This is a simple, minimalistic tool to help you send, and manage emails.
+        </template>
+
+        <hr class="my-4">
+
+        <p>
+          Give it a ride and have fun :)
+        </p>
+      </b-jumbotron>
+    </div>
   </div>
 </template>
 
